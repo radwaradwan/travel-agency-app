d009cfc293880b292a62fe8fab5acd138b89bfe6
radwa radwan <radwaradwan142@gmail.com>
2024-04-15 19:46:50 +0200

    update downloads component

--- src/Components/Downloads/Downloads.js
+++ src/Components/Downloads/Downloads.js
@@ -6,7 +6,7 @@ import qr from '../../assets/img/QR.svg'
 function Downloads() {
     return (
         <section>
-            <div className='container d-flex justify-content-between mt-5 mb-2'>
+            <div className='container d-flex justify-content-md-between flex-md-row flex-column mt-5 mb-2 align-items-md-center'>
                 <div>
                     <p className={styles.titleDownloads}>Go Further With The Mina Voyages</p>
                     <div className={styles.bodyDownloads}>
@@ -16,11 +16,11 @@ function Downloads() {
                     <p className='text-muted'>Secured By Europe GTP</p>
                 </div>
                 <div>
-                    <div className='mb-4'>
+                    <div className='d-flex justify-content-end mb-4 me-md-0 me-3'>
                         <img src={apple} alt='apple download' className='me-4'/>
                         <img src={qr} alt='QR'/>
                     </div>
-                    <div>
+                    <div className='d-flex justify-content-end me-md-0 me-3'>
                         <img src={googlePlay} alt='googlePlay download' className='me-4'/>
                         <img src={qr} alt='QR'/>
                     </div>
