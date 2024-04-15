db70952e2e09aabba4e9267aeeb5818bfb127bb1
radwa radwan <radwaradwan142@gmail.com>
2024-04-15 20:02:26 +0200

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -14,8 +14,8 @@ import mail from '../../assets/img/mail.svg'
 function Footer() {
     return (
         <section className='container mt-5'>
-            <div className='row mb-5'>
-                <div className={`col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
+            <div className='row mb-5 '>
+                <div className={`col-6 col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
                     <p className='fw-bold text-black'>About Us</p>
                     <p>Our Story</p>
                     <p>Work with us</p>
@@ -23,7 +23,7 @@ function Footer() {
                     <p>Privacy & Security</p>
                 </div>
 
-                <div className={`col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
+                <div className={`col-6 col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
                     <p className='fw-bold text-black'>We Offer</p>
                     <p>Trip Sponsorship</p>
                     <p>Last Minutes Flights</p>
@@ -31,7 +31,7 @@ function Footer() {
                     <p>AI-Driven Search</p>
                 </div>
 
-                <div className={`col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
+                <div className={`col-6 col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
                     <p className='fw-bold text-black'>Headquarters</p>
                     <p>England</p>
                     <p>France</p>
@@ -39,7 +39,7 @@ function Footer() {
                     <p>Iceland</p>
                 </div>
 
-                <div className={`col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
+                <div className={`col-6 col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
                     <p className='fw-bold text-black'>Travel Blogs</p>
                     <p>Bali Travel Guide</p>
                     <p>Sri Travel Guide</p>
@@ -47,7 +47,7 @@ function Footer() {
                     <p>Swiss Travel Guide</p>
                 </div>
 
-                <div className={`col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
+                <div className={`col-6 col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
                     <p className='fw-bold text-black'>Activities</p>
                     <p>Tour Leading</p>
                     <p>Cruising & sailing</p>
@@ -55,35 +55,35 @@ function Footer() {
                     <p>Kayaking</p>
                 </div>
 
-                <div className={`col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
+                <div className={`col-6 col-md-2 d-flex flex-column text-muted ${styles.footerSection}`}>
                     <p className='fw-bold text-black'>Service</p>
                     <p>Report Error</p>
                     <p>Ask online</p>
                     <p>Travel insurance</p>
                 </div>
             </div>
-            <div className={`row mb-5 d-flex justify-content-between align-items-center`}>
-                <div className='col-md-4 d-flex justify-content-between pe-5'>
+            <div className='row mb-5 d-flex justify-content-md-between flex-md-row flex-column align-items-center'>
+                <div className='col-md-4 d-flex justify-content-between pe-md-5'>
                     <img src={visa} alt='visa pay'/>
                     <img src={amex} alt='amex pay'/>
                     <img src={master} alt='master card pay'/>
                     <img src={paypal} alt='paypal pay'/>
                 </div>
 
-                <div className='col-md-3'>
-                    <div className='px-5 d-flex justify-content-between'>
-                        <img src={linkedin} alt='linkedin'/>
-                        <img src={telegram} alt='telegram'/>
-                        <img src={twitter} alt='twitter'/>
-                        <img src={facebook} alt='facebook'/>
-                        <img src={instagram} alt='instagram'/>
+                <div className='col-md-3 my-4'>
+                    <div className='px-md-5 d-flex justify-content-md-between justify-content-center '>
+                        <img src={linkedin} alt='linkedin' className='me-4 me-md-0'/>
+                        <img src={telegram} alt='telegram' className='me-4 me-md-0'/>
+                        <img src={twitter} alt='twitter' className='me-4 me-md-0'/>
+                        <img src={facebook} alt='facebook' className='me-4 me-md-0'/>
+                        <img src={instagram} alt='instagram' className='me-4 me-md-0'/>
                     </div>
                 
                 </div>
 
                 <div className='col-md-5'>
                     {/*<label className='ms-5'>Email</label>*/}
-                    <div className="input-group px-5">
+                    <div className="input-group px-md-5">
                         <img src={mail} alt='mail' className={`input-group-text ${styles.imgIcon}`}/>
                         <input type="text" className={`form-control py-2 ${styles.emailInput}`} placeholder="Enter Your Email" id='email'/>
                         <button className={`btn btn-outline-secondary btn-primary text-white ${styles.subscribeBTN}`}>Subscribe</button>
