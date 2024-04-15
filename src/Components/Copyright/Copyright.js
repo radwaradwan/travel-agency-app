9f4e5de9847d3aca1b3c75fa8eb7ba0b90eb6ee9
radwa radwan <radwaradwan142@gmail.com>
2024-04-15 20:11:18 +0200

    Update Copyright Component

--- src/Components/Copyright/Copyright.js
+++ src/Components/Copyright/Copyright.js
@@ -7,20 +7,20 @@ import phone from '../../assets/img/phoneFooter.svg'
 function Copyright() {
     return (
         <section>
-            <div className={`d-flex justify-content-center p-3 ${styles.CopyrightSection}`}>
-                <div className='col-md-auto d-flex  me-4'>
+            <div className={`d-flex justify-content-center flex-md-row flex-column p-3 ${styles.CopyrightSection}`}>
+                <div className='col-md-auto d-flex justify-content-center me-md-4'>
                     <img src={copyright} alt='copyright'/>
                     <p className='text-muted mt-3 mx-1'>Copyright Mina Voyages</p>
                 </div>
-                <div className='col-md-auto d-flex  me-4'>
+                <div className='col-md-auto d-flex justify-content-center me-md-4'>
                     <img src={email} alt='email'/>
                     <p className='text-muted mt-3 mx-1'>minavoyages@gmail.com</p>
                 </div>
-                <div className='col-md-auto d-flex  me-4'>
+                <div className='col-md-auto d-flex justify-content-center me-md-4'>
                     <img src={location} alt='location'/>
                     <p className='text-muted mt-3 mx-1'>123 Oxford Street,London</p>
                 </div>
-                <div className='col-md-auto d-flex me-4'>
+                <div className='col-md-auto d-flex justify-content-center me-md-4'>
                     <img src={phone} alt='phone'/>
                     <p className='text-muted mt-3 mx-1'>+44 20 7123 4567</p>
                 </div>
